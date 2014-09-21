86a19a25112ecadabaa1aecfe12b90ab1f399a1b
Ethan Keller <ethan.keller@gmail.com>
2014-09-21 09:50:20 -0400

    last commit

--- PS2/ps2.ml
+++ PS2/ps2.ml
@@ -5,7 +5,6 @@ type 'a exprTree =
   | Unop of ('a -> 'a) * 'a exprTree 
   | Binop of ('a -> 'a -> 'a) * 'a exprTree * 'a exprTree
 
-  (**x:a'exprTree = (Binop ((+), Val 3, Unop ((~-), Binop ((/), Val 5, Val 2))))**)
 (*returns the number of function app in an exprTree*)
 let rec count_ops (et : 'a exprTree) : int = 
         match et with
@@ -18,42 +17,55 @@ let rec count_ops (et : 'a exprTree) : int =
 (*returns an exprTree representing the execution of fact n (as defined in the
   write-up for exercise 5)*)
 let rec make_fact_tree (n : int) : int exprTree =
-  failwith "I mean, why would a poptart want to live inside a toaster, Rick? I 
-  mean, that would be like the scariest place for them to live. You know what I 
-  mean?"
+  if n=0||n=1 then Val 1 else
+  Binop (( * ), Val n, make_fact_tree(n-1))
   
 (*computes the expression represented by [et]*)
 let rec eval (et : 'a exprTree) : 'a =
-  failwith "You're missing the point, Morty. Why would he drive a smaller 
-  toaster with wheels? I mean does your car look like a smaller version of your 
-  house? No."
+  match et with 
+  | Val x-> x
+  | Unop (u, y) -> u (eval(y))
+  | Binop (b, w, v) -> b (eval(w)) (eval(v))
 
 (* PART 2: FOLDING*)
 
+(*Computes the product of all the floats in a list, ie h1*h2*h3...*t*)
 let product (lst : float list) : float =
   match lst with
   [] -> 1.0
   | lst -> List.fold_left (fun acc x -> acc*.x) 1.0 lst
 
+(*Concatenates a list of strings together using fold_left, ie h1^h2^h3...^t*)
 let concat_left (lst : string list) : string = 
-  failwith "SQUIRREL!!"
+  List.fold_left (fun s1 s2-> s1^s2) "" lst
 
+(*Concatenates a list of strings together using fold_right, ie h1^h2^h3...^t*)
 let concat_right (lst : string list) : string = 
-  failwith "POINT!"
+  List.fold_right (fun s1 s2-> s1^s2) lst ""
 
+(*The input function f is applied to the index (counting from 0) of the element 
+in the list as the first argument and the element itself as the second 
+argument.*)
 let mapi_lst (f: (int -> 'a -> 'b)) (lst: 'a list) : 'b list =
   let length lst: int = List.fold_right (fun x acc -> acc+1) lst 0
 in
   List.rev (List.fold_left (fun acc x -> (f (length acc) x)::acc) [] lst)
   
-
+(*Appends an outline numbering to each string of the list that includes the 
+*index number +1, (starting with 1), followed by a period followed by a space. 
+*Ex: outline ["A";"B";"C"] = ["1. A"; "2. B"; "3. C"].*)
 let outline (lst: string list) : string list =
   mapi_lst (fun i a -> (string_of_int (i+1))^". "^a) (lst: 'a list)
 
-      
+(*Performs a fold_right, but the accumulator is a list containing all the values 
+*the accumulator takes on as f is applied to each element in lst starting with 
+*the right-most element*)     
 let scan_right (f: 'a -> 'b -> 'a) (acc: 'a) (lst: 'b list) : 'a list =
   List.rev (List.fold_right (fun x acc -> (f x (List.hd acc))::acc) lst [acc])
-      
+
+(*Performs a fold_left, but the accumulator is a list containing all the values 
+*the accumulator takes on as f is applied to each element in lst starting with 
+*the left-most element*)       
 let scan_left (f: 'a -> 'b -> 'a) (acc: 'a) (lst: 'b list) : 'a list =
   List.rev (List.fold_left (fun acc x -> (f x (List.hd acc))::acc) [acc] lst)
 
@@ -68,6 +80,11 @@ let countup (n:int) : int list =
     else countup' (i-1) (i::l)
   in countup' n []
 
+(*Takes in a positive int n and creates a list of each factorial of every 
+*integer starting from 1 and ending with n. 
+*Precondition: n > 0 and !n < max int.
+*Postcondition: if !n happens to be larger than max int, may return 0, 
+*negatives or other nonsense.*)
 let fact_list (n: int) : int list =
   scan_left (fun acc x -> x*acc) 1 (List.tl (countup n))
 
@@ -79,65 +96,75 @@ type matrix = vector list
 
 exception MatrixFailure of string
 
-(* Takes in a matrix and returns a int*int with number of rows in the 1st place 
-  *and columns in the 2nd: (Nrows,Ncols). Requires rectangular matrix*)
-let matrix_size (m : matrix) : int*int = 
-  match m with
-  [] -> (0,0)
-  | h::t -> ((List.length m),(num_cols m))
-
 (*Takes in a rectangular matrix and returns the number of columns in the 
-*matrix.*)
+*matrix.
+*Preconditions: matrix must be rectangular, meaning all vector lengths are equal
+*Postcondition: the number of vectors in the matrix or -1 if the matrix is not 
+*rectangular*)
 let num_cols (m : matrix) : int =
   match m with 
   [] -> 0
-  | h::t -> List.fold_left (fun acc x -> if (acc=List.length x) then acc else 
-    raise (MatrixFailure "Matrix is not rectangular")) (List.length h) m i  
+  | h::t -> List.fold_left (fun acc x -> if (acc=List.length x) then acc 
+    else -1) (List.length h) m
 
-(*Takes in a int list list and returns a boolean of whether it is a rectangular 
-*matrix or not.*)
+(*Takes in a matrix and returns true if the matrix is rectangular and false if 
+*not.*)
 let is_matrix (m : matrix) : bool =
-  if (num_cols m) = (MatrixFailure "Matrix is not rectangular") then false 
-  else true
+  if (num_cols m) = -1 then false else true
 
-(*Takes in a rectangular matrix and returns a printout of the whole matrix.*)  
+(*Takes in a rectangular matrix and returns a printout of the whole matrix, 
+*with each vector on a new line and spaces between the elements.*)  
 let show (m : matrix) : unit = 
-  if (is_matrix)=false then (MatrixFailure "Matrix is not rectangular") else
-  List.fold_left (fun acc x -> print_endline (List.fold_left (fun acc x -> 
+  if (is_matrix m)=false then raise (MatrixFailure "Matrix is not rectangular") 
+  else List.fold_left (fun acc x -> print_endline (List.fold_left (fun acc x -> 
   (string_of_int x)^" "^acc) "" (List.rev x))) () m
 
 (*Takes in a rectangular matrix and a vector with the same number of elements 
-*as the number of rows in the input matrix.*)
+*as the number of rows in the input matrix.
+*Precondition: vector c must have the same number of elements as the number of 
+*rows in matrix m.
+*Postcondition: returns a matrix with the vector elements appended as a new 
+*column to matrix m.*)
 let insert_col (m : matrix) (c : vector) : matrix = 
-  if (is_matrix)=false then (MatrixFailure "Matrix is not rectangular") 
-  else if (List.length c) <> (List.length m) then 
+  if (is_matrix m)=false then raise (MatrixFailure "Matrix is not rectangular") 
+  else if (List.length c) <> (List.length m) then raise
     (MatrixFailure "Number of elements in c and rows in m do not match") else
     List.rev (List.fold_left2 (fun acc v ce -> (v@[ce])::acc) [] m c)
 
 (*Takes in a rectangular matrix and switches the rows for columns, transposing 
 *it.*)
 let transpose (m : matrix) : matrix = 
-  if (is_matrix)=false then (MatrixFailure "Matrix is not rectangular") 
+  if (is_matrix m)=false then raise (MatrixFailure "Matrix is not rectangular") 
   else match m with 
       [] -> []
-      | h::t -> let m' = List.rev 
-        (List.fold_left (fun acc x -> [x]::acc) [] h)
+      | h::t -> 
+        let m' = List.rev (List.fold_left (fun acc x -> [x]::acc) [] h) in
       List.fold_left (fun acc x -> insert_col acc x) m' t
 
 (*Takes in two rectangular matrices of equal size and returns a matrix of 
 *equal size composed of the sum of each element of the first two in the 
-*corresponding locations.*)
+*corresponding locations.
+*Preconditions: The matrices m1 and m2 must be of identical size.*)
 let add_matrices (m1 : matrix) (m2 : matrix) : matrix = 
-  List.rev (List.fold_left2 (fun accv v1 v2 -> List.rev (List.fold_left2 
+  if ((is_matrix m1)=false || (is_matrix m2)=false) then raise (MatrixFailure 
+    "Matrix is not rectangular")
+  else List.rev (List.fold_left2 (fun accv v1 v2 -> List.rev (List.fold_left2 
     (fun acce e1 e2 -> (e1+e2)::acce) [] v1 v2)::accv) [] m1 m2)
 
-(*Multiplies two rectangular matrices together. Note the number of columns of 
-*the first matrix must equal the number of rows of the second.*)
+(*Multiplies two rectangular matrices together. 
+*Preconditions: the number of columns of the first matrix must equal the number 
+of rows of the second.
+*Postcondition: the returned matrix has as many rows as m2 has columns and as 
+*many columns as m1 has rows*)
 let multiply_matrices (m1 : matrix) (m2 : matrix) : matrix = 
   let dt_prd v1 v2 = List.fold_left2 (fun acc e1 e2 -> 
-    (e1*e2)+acc) 0 v1 v2 in
-  List.rev (List.fold_left (fun acc r -> (List.rev (List.fold_left 
-    (fun r' c -> (dt_prd c r)::r') [] (transpose m2)))::acc) [] m1) 
+      (e1*e2)+acc) 0 v1 v2 in
+  if ((is_matrix m1)=false || (is_matrix m2)=false) then raise (MatrixFailure 
+    "Matrix is not rectangular") else if (num_cols m1) <> (List.length m2) then
+    raise (MatrixFailure "The number of columns of the first matrix does not 
+      equal the number of rows of the second")
+    else List.rev (List.fold_left (fun acc r -> (List.rev (List.fold_left 
+      (fun r' c -> (dt_prd c r)::r') [] (transpose m2)))::acc) [] m1) 
 
 (* PART 4: PATTERN MATCHING *)
 
@@ -159,12 +186,12 @@ type value =
 type bindings = (string * value) list option
 
 (*1. *************************************************************************)
-(*Precondition: f1 must take in type unit (unit->int), and f2 takes in type 
-string (string->int) and both return an integer. 
-Postcondition: returns an integer. Takes in a pattern and functions f1 and f2. 
-Applies f1 to wildcard patterns and f2 to variable patterns within Tuple or 
-Struct patterns.*)
-let rec z f1 f2 p =
+(*Takes in a pattern p and functions f1 and f2. Applies f1 to wildcard patterns 
+*and f2 to variable patterns that can be within Tuple or Struct patterns.
+*Precondition: f1 must be of type (unit->int), and f2 must be of type 
+*(string->int). 
+*Postcondition: returns an integer.*)
+let rec z (f1: unit->int) (f2: string->int) (p: pat) : int =
   let r = z f1 f2 in
     match p with
     | WCPat -> f1 ()
@@ -177,20 +204,21 @@ let rec z f1 f2 p =
 let count_wcs (p: pat) : int = 
     z (fun () -> 1) (fun s -> 0) p
 
-(*counts the number of wildcards in a pattern, and adds that quantity to the sum
-of the lengths of the variable names used in the pattern*)
+(*counts the number of wildcards in a pattern, and adds that quantity to the 
+*sum of the lengths of the variable names used in the pattern*)
 let count_wcs_and_var_lengths (p: pat) : int = 
   z (fun () -> 1) (fun s -> String.length s) p
 
-(*counts how oftern a variable occurs in a pattern*)
+(*counts how often a variable occurs in a pattern*)
 let count_var (var_name: string) (p: pat) : int = 
   z (fun () -> 0) (fun s -> if (s=var_name) then 1 else 0) p
 
 (*2. *************************************************************************)
 
 (*Takes in a type pat and returns the names of all the variables in the pattern 
-in no specified order. Precondition: p is a valid pattern. 
-Postcondition: a list of the strings of the variable names)*)
+*in no specified order. 
+*Precondition: p is a valid pattern. 
+*Postcondition: a list of the strings of the variable names)*)
 let rec extract_names (p: pat) : string list = 
   match p with
     | VarPat x -> [x]
@@ -199,8 +227,7 @@ let rec extract_names (p: pat) : string list =
     | _ -> []
 
 (*Takes a list of any type and returns true if the list contains duplicates, 
-and false if all elements are unique. []->false. Precondition: A list. 
-Postcondition: boolean.)*)
+*and false if all elements are unique. []->false.*)
 let has_dups (l: 'a list) : bool = 
   (*checks if element e is equal to any elements is l*)
   let rec chk_for_el (e: 'a) (l:'a list) : bool =
@@ -213,17 +240,15 @@ let has_dups (l: 'a list) : bool =
   |h1::h2::tl -> if (chk_for_el h1 (h2::tl)) then true else (chk_for_el h2 tl)
   |hd::tl -> false
 
-(*Checks if all the variable names in a pattern are all unique. 
-*Precondition: a valid pattern. Postcondition: a boolean.*)
+(*Checks if all the variable names in a pattern are all unique. Returns true iff
+*all names are unique, false otherwise.*)
 let all_vars_unique (p: pat) : bool = 
   not (has_dups (extract_names p))
 (*3. *************************************************************************)
 
-(*Passes elements in the original list into f and combines the output, minus 
-*the option into a single list. If any f a -> None then return None, else return 
-*Some 'b list.
-*Precondition: 
-*Postcondition:*)
+(*Passes elements in the original list into f and combines the output without 
+*the option into a single list in no specified order. If any f a -> None then 
+*return None, else return Some 'b list.*)
 let all_answers (f: 'a -> 'b list option) (l: 'a list) : 'b list option =
   let rec comblist (acc: 'b list) (l: 'b list option list) : 'b list option =
     match l with
@@ -238,9 +263,7 @@ comblist [] (List.map f l)
 (*Matches value v with pattern p and returns the bindings produced as a result 
 *of the match. If no match is produced, return None. If a match but no bindings 
 *are produced, return Some []. If a match and bindings are produced, return 
-*Some l where l is the list of bindings.
-*Precondtion:
-Postcondition:*)
+*Some l where l is the list of bindings.*)
 let rec match_pat (v,p) : bindings =
   match (v,p) with
   | _ , WCPat -> Some []
@@ -260,26 +283,24 @@ let rec match_pat (v,p) : bindings =
 (*5. *************************************************************************)
 exception NoAnswer
 
-(*Finds the first occurence of some when each element a of l is passed into f
-*Precondition:
-*Postcondition:*)
+(*Returns the contents of the first occurence of Some as each element of l 
+is passed into f. If Some is never produced, return exception NoAnswer*)
 let rec first_answer (f: 'a -> 'b option) (l: 'a list) =
   match l with
   |hd::tl -> if ((f hd)=None) then first_answer f tl else 
     (match (f hd) with
     |Some x -> x
-    |_ -> NoAnswer)
-  |[] -> NoAnswer
+    |_ -> raise NoAnswer)
+  |[] -> raise NoAnswer
 
 
 (*6. *************************************************************************)
 
-(*Performs a pattern matching on a list of patters, ps and a value v. Returns 
+(*Performs a pattern matching on a list of patterns, ps and a value v. Returns 
 *the bindings produced from the first successful match. If no patterns match, 
 *returns None, and if a pattern matches but no bindings produced, 
-*returns Some[].
-*Precondition:
-*Postcondition:*)
+*returns Some[]. If a match and bindings are produced, return 
+*Some l where l is the list of bindings produced by the first match.*)
 let rec match_pats ((v: value), (ps: pat list)) : bindings =
   match ps with
   [] -> None
--- PS2/ps2_test.ml
+++ PS2/ps2_test.ml
@@ -1,28 +1,116 @@
 open Assertions
 open Ps2
 
-(*Tests the count_ops function*)
 TEST_UNIT "count_ops" =
-	let _ = assert_true (0 = count_ops(Val 2)) in
+    let _ = assert_true (0 = count_ops(Val 2)) in
 	let _ = assert_true (1 = count_ops(Binop ((+), Val 2, Val 3))) in
 	let _ = assert_true (3 = count_ops(Binop ((+), Val 3, Unop ((~-), Binop ((/), Val 5, Val 2))))) in
 ()
 
-(*
-WCPat
-VarPat "hi"
-UnitPat
-ConstPat 1
-TuplePat [WCPat,WCPat]
-StructorPat ("Fred", VarPat "hi")
-ConstVal 1
-UnitVal
-TupleVal [UnitVal,UnitVal]
-StructorVal ("Bob", UnitVal)
-*)
+(*I also test make_fact_tree by calling eval on it below.*)
+TEST_UNIT "make_fact_tree" =
+	let _ = assert_true(Val 1 = make_fact_tree(0)) in 
+	let _ = assert_true(Val 1 = make_fact_tree(1)) in
+	(* let _ = assert_true(Binop (( * ), Val 2, Val 1)  = make_fact_tree(2)) in*
+	 * We tested the rest of the function calls in utop, and got convincing *
+	 * results.  Here are some:
+	 * utop # make_fact_tree(3);;
+     * - : int exprTree = Binop (<fun>, Val 3, Binop (<fun>, Val 2, Val 1))
+     * utop # make_fact_tree(2);;
+     * - : int exprTree = Binop (<fun>, Val 2, Val 1) *)   
+
+	()
+
+(*This function helps with testing eval,
+ * Returns n+n-1+...+1, with eval(0)=1*)
+let rec make_add_tree(n:int) : int exprTree = 
+  if n=0||n=1 then Val 1 else
+  Binop (( + ), Val n, make_fact_tree(n-1))
+
+TEST_UNIT "eval" =
+	let _ = assert_true(1 = eval(make_fact_tree(0))) in 
+	let _ = assert_true(1 = eval(make_fact_tree(1))) in
+	let _ = assert_true(2 = eval(make_fact_tree(2))) in
+	let _ = assert_true(6 = eval(make_fact_tree(3))) in
+	let _ = assert_true(24 = eval(make_fact_tree(4))) in
+	(*Made the make_add_tree to help with testing, see ps2.ml*)
+	let _ = assert_true(1 = eval(make_add_tree(0))) in 
+	let _ = assert_true(3 = eval(make_add_tree(2))) in
+	let _ = assert_true(10 = eval(make_add_tree(4))) in
+	(*We tested, rasing an division by zero exception in utop
+
+	utop # let x33=(Binop((/), Val 1, Val 0));;
+	val x33 : int exprTree = Binop (<fun>, Val 1, Val 0)                           
+	 ─( 23:13:52 )─< command 36 >─────────────────────────────────────{ counter: 0 }─utop # eval(x33);;
+	Exception: Division_by_zero. 
+	*)
+	()
+
+
+TEST_UNIT "product" = 
+    let _ = assert_true(product [777.5; 4.] = 3110.)(*given*) in
+    let _ = assert_true(product [4.; 777.5] = 3110.)(*switch order*) in
+    let _ = assert_true(product [] = 1.) (*given*) in
+    let _ = assert_true(product [0.0;4.;1247.4] = 0.) (*zero*) in
+    let _ = assert_true(product [-1.]= (-1.)) (*negative*) in
+    () 
+
+TEST_UNIT "concat_left" = 
+    assert_true(concat_left[]="");(**)
+    assert_true(concat_left[""]="");(**)
+    assert_true(concat_left["";""]="");(**)
+    assert_true(concat_left["cs";"3110"]="cs3110");(**)
+    assert_true(concat_left["3110";"cs"]="3110cs");(**)
+    assert_true(concat_left["3110";"cs";"uiop"]="3110csuiop");(**)
+    assert_true(concat_left["";"31";"";"10";""] = "3110");
+
+TEST_UNIT "concat_right" =
+    assert_true(concat_right[]="");(**)
+    assert_true(concat_right[""]="");(**)
+    assert_true(concat_right["";""]="");(**)
+    assert_true(concat_right["cs";"3110"]="cs3110");(**)
+    assert_true(concat_right["3110";"cs"]="3110cs");(**)
+    assert_true(concat_right["3110";"cs";"uiop"]="3110csuiop");(**)
+    assert_true(concat_right ["";"31";"";"10";""] = "3110");
+
+TEST_UNIT "mapi_list: PS2 P2 EX 3" = 
+    assert_true(mapi_lst (+) [3;0;-1;-3] = [3;1;1;0]);
+    assert_true(mapi_lst (+) [0;0;0] = [0;1;2]);
+    assert_true(mapi_lst ( * ) [3;0;-1;-3] = [0;0;-2;-9]);
+    assert_true(mapi_lst (+) [] = []);
+    assert_true(mapi_lst ( - ) [0] = [0]);
+    assert_true(mapi_lst ( - ) [0;0;0] = [0;1;2]);
+
+TEST_UNIT "outline" = 
+    assert_true(outline [] = []);
+    assert_true(outline["one";"";"three";""]=["1. one"; "2. "; "3. three"; "4. "] )
+
+TEST_UNIT "scan_left" =
+    assert_true(scan_left (+) 0 [1;2;3] = [0;1;3;6]);
+    assert_true(scan_left (^) ""  ["";"m"] = ["";"";"m"]);
+
+TEST_UNIT "scan_right" = 
+   assert_true(scan_right (+) 0 [1;2;3] = [0;3;5;6]);
+   assert_true(scan_right (^) "swag" ["zar";"doz"] = ["swag";"dozswag";"zardozswag"]);
+
+ TEST_UNIT "show" =
+   assert_true(show [] = ());
+
+TEST_UNIT "insert_col" =
+    assert_true(insert_col [[1;2;3];[4;5;6]] [6;7] = [[1;2;3;6];[4;5;6;7]]);
+
 (*Tests the count_ops function*)
+TEST_UNIT "count_ops" =
+	let _ = assert_true (0 = count_ops(Val 2)) in
+	let _ = assert_true (1 = count_ops(Binop ((+), Val 2, Val 3))) in
+	let _ = assert_true (3 = count_ops(Binop ((+), Val 3, Unop 
+		((~-), Binop ((/), Val 5, Val 2))))) in
+()
+
+(*Tests the count_wcs function*)
 TEST_UNIT "count_wcs" =
 	let _ = assert_true (1 = count_wcs WCPat) in
+	let _ = assert_true (0 = count_wcs (VarPat "howdy")) in
 	let _ = assert_true (2 = count_wcs (TuplePat [WCPat;WCPat])) in
 	let _ = assert_true (1 = count_wcs (StructorPat ("Fred", Some WCPat))) in
 ()
@@ -30,6 +118,7 @@ TEST_UNIT "count_wcs" =
 (*Tests the count_wcs_and_var_lengths function*)
 TEST_UNIT "count_wcs_and_var_lengths" =
 	let _ = assert_true (1 = count_wcs_and_var_lengths WCPat) in
+	let _ = assert_true (5 = count_wcs_and_var_lengths (VarPat "howdy")) in
 	let _ = assert_true (2 = count_wcs_and_var_lengths 
 		(TuplePat [WCPat;WCPat])) in
 	let _ = assert_true (4 = count_wcs_and_var_lengths (StructorPat ("Fred", 
@@ -39,15 +128,26 @@ TEST_UNIT "count_wcs_and_var_lengths" =
 (*Tests the count_var function*)
 TEST_UNIT "count_var" =
 	let _ = assert_true (0 = count_var "hi" (WCPat)) in
+	let _ = assert_true (0 = count_var "hi" (VarPat "howdy")) in
 	let _ = assert_true (0 = count_var "hi" (TuplePat [WCPat;WCPat])) in
-	let _ = assert_true (1 = count_var "Bill" (StructorPat ("Fred", Some (VarPat 
-		"Bill"))))in
+	let _ = assert_true (0 = count_var "Steve" (StructorPat 
+		("Fred", Some (VarPat "Bill"))))in
+	let _ = assert_true (0 = count_var "hi" (VarPat "howdy")) in
+	let _ = assert_true (2 = count_var "howdy" 
+		(TuplePat [VarPat "howdy";VarPat "howdy"])) in
+	let _ = assert_true (1 = count_var "Steve" (StructorPat 
+		("Steve", Some (VarPat "Steve")))) in
+	let _ = assert_true (1 = count_var "Steve" (StructorPat 
+		("Steve", Some (StructorPat ("Steve", Some (VarPat "Steve")))))) in
 ()
 
 (*Tests the extract_names function*)
 TEST_UNIT "extract_names" =
 	let _ = assert_true ([] = extract_names (WCPat)) in
+	let _ = assert_true (["howdy"] = extract_names (VarPat "howdy")) in
 	let _ = assert_true ([] = extract_names (TuplePat [WCPat;WCPat])) in
+	let _ = assert_true (["howdy";"howdy"] = 
+		extract_names (TuplePat [(VarPat "howdy");(VarPat "howdy")])) in
 	let _ = assert_true (["Fred";"Bill"] = extract_names (StructorPat ("Fred", 
 		Some (VarPat "Bill"))))in
 ()
@@ -56,6 +156,7 @@ TEST_UNIT "extract_names" =
 TEST_UNIT "has_dups" =
 	let _ = assert_true (false = has_dups []) in
 	let _ = assert_true (false = has_dups [WCPat]) in
+	let _ = assert_true (false = has_dups ["hi";"hello";"howdy"])in
 	let _ = assert_true (true = has_dups [WCPat;WCPat]) in
 	let _ = assert_true (true = has_dups ["hi";"hi"]) in
 	let _ = assert_true (true = has_dups ["hi";"hello";"hi"])in
@@ -82,11 +183,10 @@ TEST_UNIT "all_answers" =
 
 (*Tests the match_pat function*)
 TEST_UNIT "match_pat" =
-	let _ = assert_true (Some [1; 2; 3; 1; 2; 1] = all_answers (fun x -> 
-		Some (countup x)) [1;2;3]) in
-	let _ = assert_true (None = all_answers (fun x -> None) [1;2;3]) in
-	let _ = assert_true (None = all_answers (fun x -> if x<2 then Some [1;2;3] 
-		else None) [1;2;3]) in
+	let _ = assert_true (Some [] = match_pat (UnitVal,WCPat)) in
+	let _ = assert_true (Some [] = 
+		match_pat (StructorVal ("Jane",Some (ConstVal 5)), 
+		StructorPat ("Jane",Some (ConstPat 5)))) in
 ()
 
 let () = Pa_ounit_lib.Runtime.summarize ()
