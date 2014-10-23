52b46e2ec0f87f05dc23013abce13ad15c44faae
Ethan Keller <ethan.keller@gmail.com>
2014-10-23 15:02:00 -0400

    P1 completely done with test cases.

--- PS4/PS4New/mutability_test.ml
+++ PS4/PS4New/mutability_test.ml
@@ -21,7 +21,12 @@ TEST_UNIT  "tabulate" =
 
 TEST_UNIT  "fold_left_imp" = 
   assert_true ((fold_left_imp (+) 0 [1;2;3;4]) = 10);
-  assert_true ((fold_left_imp (fun a x -> x::a) [] [1;2;3;4]) = [4;3;2;1])
+  assert_true ((fold_left_imp (fun a x -> x::a) [] [1;2;3;4]) = [4;3;2;1]);
   ()  
+
+TEST_UNIT  "exercise 4" = 
+  assert_false (List.map zardoz (List.rev lst) = 
+    List.rev (List.map zardoz lst));
+  ()
 
 let () = Pa_ounit_lib.Runtime.summarize ()
