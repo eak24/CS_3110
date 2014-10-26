6b69ce3928ce69207ed0339aaeadda2c8e58d637
Ethan Keller <ethan.keller@gmail.com>
2014-10-25 23:54:22 -0400

    lastpush

--- PS4/PS4New/interpreter/eval.ml
+++ PS4/PS4New/interpreter/eval.ml
@@ -13,9 +13,6 @@ and value =
 and binding = value ref Environment.binding
 and environment = value ref Environment.environment
 
-(* Exceptions*)
-exception Error of string 
-
 (* Parses a datum into an expression. *)
 let rec read_expression (input : datum) : expression =
 
@@ -35,7 +32,7 @@ let rec read_expression (input : datum) : expression =
     | Atom (Identifier id) -> [Identifier.variable_of_identifier id]
     | Cons (car,cdr) -> 
         (cons_to_var_list car) @ (cons_to_var_list cdr)
-    | _ -> raise (Error "cons_to_var_list, this should be a variable")
+    | _ -> failwith "cons_to_var_list, this should be a variable"
   in
 
   let rec let_binding_helper (dat : datum) : let_binding list = 
@@ -49,17 +46,17 @@ let rec read_expression (input : datum) : expression =
         ((Identifier.variable_of_identifier id),(read_expression cdr'))::
         (let_binding_helper cdr)
 
-    | _ -> raise (Error "let_binding_helper warning, datum must be let binding")
+    | _ -> failwith "let_binding_helper warning, datum must be let binding" 
 in
 
   match input with
-  | Nil -> raise (Error "Unknown expression form")
+  | Nil -> failwith "Unknown expression form"
 
   (* Self evaluating matches*)
   | Atom (Identifier id) when Identifier.is_valid_variable id ->
     ExprVariable (Identifier.variable_of_identifier id)
 
-  | Atom (Identifier id) -> raise (Error "Keyword is invalid variable name")
+  | Atom (Identifier id) -> failwith "Keyword is invalid variable name"
   | Atom (Boolean b) -> ExprSelfEvaluating (SEBoolean b)
   | Atom (Integer i) -> ExprSelfEvaluating (SEInteger i)
 
@@ -124,32 +121,32 @@ let rec initial_environment () : environment =
       (fun vlst env ->
         match vlst with 
         | [ValDatum (Cons (car, cdr))] -> ValDatum car
-        | _ -> raise (Error "Invalid arguments for car"))))) in
+        | _ -> failwith "Invalid arguments for car")))) in
   
   let e_cdr = Environment.add_binding e_car (Identifier.variable_of_identifier 
     (Identifier.identifier_of_string "cdr"), ref (ValProcedure (ProcBuiltin
       (fun vlst env ->
         match vlst with 
         | [ValDatum (Cons (car, cdr))] -> ValDatum cdr
-        | _ -> raise (Error "Invalid arguments for cdr"))))) in
+        | _ -> failwith "Invalid arguments for cdr")))) in
   
   let e_cons = Environment.add_binding e_cdr (Identifier.variable_of_identifier 
     (Identifier.identifier_of_string "cons"), ref (ValProcedure (ProcBuiltin
       (fun vlst env ->
         match vlst with 
         | [(ValDatum car);(ValDatum cdr)] -> ValDatum (Cons (car , cdr))
-        | _ -> raise (Error "Invalid arguments for cons"))))) in
+        | _ -> failwith "Invalid arguments for cons")))) in
   
   let e_plus = Environment.add_binding e_cons 
     (Identifier.variable_of_identifier 
     (Identifier.identifier_of_string "+"), ref (ValProcedure (ProcBuiltin
       (fun vlst env -> 
         match vlst with
-        | [] -> raise (Error "+ expects atleast 1 argument")
+        | [] -> failwith "+ expects atleast 1 argument"
         | lst -> ValDatum (Atom (Integer (List.fold_left (fun acc x -> 
           match x with
           | ValDatum (Atom (Integer i)) -> i + acc
-          | _ -> raise (Error "+ expects all integer arguments" )) 0 lst))))))) 
+          | _ -> failwith "+ expects all integer arguments" ) 0 lst))))))) 
     in
 
   let e_mult = Environment.add_binding e_plus 
@@ -157,11 +154,11 @@ let rec initial_environment () : environment =
     (Identifier.identifier_of_string "*"), ref (ValProcedure (ProcBuiltin
       (fun vlst env -> 
         match vlst with
-        | [] -> raise (Error "* expects atleast 1 argument")
+        | [] -> failwith "* expects atleast 1 argument"
         | lst -> ValDatum (Atom (Integer (List.fold_left (fun acc x -> 
           match x with
-          | ValDatum (Atom (Integer i)) -> i + acc
-          | _ -> raise (Error "* expects all integer arguments")) 1 lst)))))))
+          | ValDatum (Atom (Integer i)) -> i * acc
+          | _ -> failwith "* expects all integer arguments") 1 lst)))))))
     in
 
   let e_equal = Environment.add_binding e_mult 
@@ -170,17 +167,17 @@ let rec initial_environment () : environment =
       (fun vlst env -> 
         match vlst with
         | v1::v2::[] -> ValDatum (Atom (Boolean (v1=v2)))
-        | _ -> raise (Error "equal? expects exactly 2 arguments"))))) in
+        | _ -> failwith "equal? expects exactly 2 arguments")))) in
 
   let e_eval = Environment.add_binding e_equal 
     (Identifier.variable_of_identifier 
     (Identifier.identifier_of_string "eval"), ref (ValProcedure (ProcBuiltin
       (fun vlst env -> 
         match vlst with
-        | [] -> raise (Error "eval expects exactly 1 argument")
-        | v1::v2::t -> raise (Error "eval expects exactly 1 argument")
+        | [] -> failwith "eval expects exactly 1 argument"
+        | v1::v2::t -> failwith "eval expects exactly 1 argument"
         | (ValDatum datum)::[] -> eval (read_expression datum) env 
-        | _ -> raise (Error "Internal error in e_eval"))))) in
+        | _ -> failwith "Internal error in e_eval")))) in
 
     e_eval
 
@@ -191,13 +188,13 @@ let rec initial_environment () : environment =
    would be a helper function for each pattern in the match
    statement. *)
 and eval (expression : expression) (env : environment) : value =
-(*
+
 (* Returns all duplicates in a given list. Returns [] for a lst with no dupes.*)
-let dupes_in_lst (lst : 'a list) : 'a list =
-  List.fold_left (fun acc x -> if List.mem x lst then x::acc else acc) [] lst 
+let rec dupes_in_lst lst acc : 'a list =
+  List.fold_left (fun acc x -> if (List.mem x lst) then x::acc else acc) acc lst 
 in
 
-*)
+
 (* Add bindings from left to right, one at a time to the environment, extending 
  * the environment for evaluating each subsequent binding expression.*)
 let extend_env_seq (env : environment) (vlst : variable list) 
@@ -206,7 +203,6 @@ let extend_env_seq (env : environment) (vlst : variable list)
     (fun acc v e -> Environment.add_binding acc (v, ref (eval e acc))) 
     env vlst elst
 in 
-(*
 
 (* Adds bindings simultaneously to the environment, using the passed in 
  * environment to evaluate each binding expression.*)
@@ -216,7 +212,7 @@ let extend_env_sim (env : environment) (vlst : variable list)
       (fun acc v e -> Environment.add_binding acc (v, ref (eval e env))) 
       env vlst elst 
 in 
-*)
+
 (* Evaluate expressions left to right. Returns the value of the last 
  * expression call.*)
 let rec eval_elst_seq (env : environment) (elst : expression list)  
@@ -225,7 +221,7 @@ let rec eval_elst_seq (env : environment) (elst : expression list)
   | h1::h2::t -> let _ = eval h1 env in 
       eval_elst_seq env (h2::t)
   | h::t -> eval h env
-  | [] -> raise (Error "Internal error: empty list match in eval_elst_seq")
+  | [] -> failwith "Internal error: empty list match in eval_elst_seq"
 in
 (*
 (* Evaluate all expressions in passed in env. Returns the value of the last 
@@ -235,7 +231,7 @@ let rec eval_elst_sim (env : environment) (elst : expression list)
   match elst with
   | h1::h2::t -> let _ = eval env h1 in eval_elst_sim env ((eval env h2)::t)
   | h::t -> eval env h2
-  | [] -> raise (Error "Internal error: empty list match in eval_elst_sim")
+  | [] -> failwith "Internal error: empty list match in eval_elst_sim"
 in
 *)
 let procCall_helper (e1 : expression) (elst : expression list) 
@@ -245,30 +241,34 @@ let procCall_helper (e1 : expression) (elst : expression list)
     (* Checks # vars = # expressions and first exp not empty.*)
     when (List.length vlst)=(List.length elst) && e1lst <> []
       -> let _ = extend_env_seq env vlst elst in eval_elst_seq env elst  
-  | ValProcedure (ProcLambda (vlst , env, e1lst)) -> raise (Error "Invalid
-    procedure call, error with ProcLambda construction.") 
+  | ValProcedure (ProcLambda (vlst , env, e1lst)) -> failwith "Invalid
+    procedure call, error with ProcLambda construction." 
   | ValProcedure (ProcBuiltin f) -> f (List.map (fun x -> eval x env) elst) env   
-  | _ -> raise 
-      (Error "Invalid procedure call, error with ProcBuiltin construction.")
-in
-(*
-let let_helper (lblst : let_binding list) (elst : expression list) : value =
-  if dupes_in_lst lblst <> [] 
-  then raise (Error "Duplicate variable names in let exp")
-  else if elst = [] then raise (Error "let binding requires at least 1 expr") 
-  else let _ = extend_env_seq env (fst lblst) (snd lblst) in 
-    eval_elst_seq env elst
+  | _ -> failwith "Invalid procedure call, error with ProcBuiltin construction."
 in
 
+let let_helper (lblst : let_binding list) (elst : expression list) : value =
+  let (lbvlst,lbelst) = List.split lblst in
+  if dupes_in_lst lbvlst [] 
+  <> [] 
+  then failwith "Duplicate variable names in let exp"
+  else if elst = [] then failwith "let binding requires at least 1 expr" 
+  else let _ = extend_env_sim env (lbvlst) (lbelst) in 
+  eval_elst_seq env elst
+in
+
+(*
 let let_star_helper lblst elst = 
   let _ = extend_env_seq env (fst lblst) (snd lblst) in 
     eval_elst_seq env elst
 in
-
+*)
+(*
 let let_rec_helper lblst elst = 
   if dupes_in_lst lblst <> [] 
-  then raise (Error "Duplicate variable names in letrec exp")
-  else let _ = extend_env_seq env (fst lblst) (snd lblst) in 
+  then failwith "Duplicate variable names in letrec exp"
+  else let (lbvlst,lbelst) = List.split lblst in
+  let _ = extend_env_seq env lbvlst lbelst in 
     eval_elst_seq env elst
 in
 *)
@@ -276,9 +276,11 @@ in
   | ExprSelfEvaluating SEBoolean b -> ValDatum (Atom (Boolean b))
   | ExprSelfEvaluating SEInteger i -> ValDatum (Atom (Integer i))
   | ExprVariable v -> if Environment.is_bound env v then 
-      !(Environment.get_binding env v) else raise (Error "Variable not bound")
+      !(Environment.get_binding env v) else failwith "Variable not bound"
   | ExprQuote d -> ValDatum d
-  | ExprLambda (vl , el) -> ValProcedure (ProcLambda (vl,env,el))
+  | ExprLambda (vl , el) -> if (*(dupes_in_lst vl) <> [] *) false
+    then failwith "Duplicate variable names in Lambda"
+    else ValProcedure (ProcLambda (vl,env,el))
   | ExprProcCall (e1 , elst) -> procCall_helper e1 elst env
 
   | ExprIf (e1 , e2 , e3) -> 
@@ -288,15 +290,14 @@ in
 
   | ExprAssignment (v , e) -> if Environment.is_bound env v then 
       let _ = (Environment.get_binding env v) := (eval e env) in ValDatum Nil
-      else raise (Error "Invalid assignment")
+      else failwith "Invalid assignment"
 
-  | ExprLet (lblst , elst) -> raise (Error "Not done") 
-    (*let_helper lblst elst*)
+  | ExprLet (lblst , elst) ->  let_helper lblst elst
 
-  | ExprLetStar (lblst , elst) -> raise (Error "Not done") 
+  | ExprLetStar (lblst , elst) -> failwith "Not done" 
     (*let_star_helper lblst elst*)
 
-  | ExprLetRec (lblst , elst) -> raise (Error "Not done") 
+  | ExprLetRec (lblst , elst) -> failwith "Not done" 
     (*let_rec_helper lblst elst*)
 
 (* Evaluates a toplevel input down to a value and an output environment in a
@@ -305,8 +306,8 @@ let eval_toplevel (toplevel : toplevel) (env : environment) :
       value * environment =
   match toplevel with
   | ToplevelExpression expression -> (eval expression env, env)
-  | ToplevelDefinition (_, _)     ->
-     failwith "I couldn't have done it without the Rower!"
+  | ToplevelDefinition (v , e) -> 
+      (ValDatum Nil , (Environment.add_binding env (v,(ref (eval e env)))))
 
 let rec string_of_value value =
   let rec string_of_datum datum =
